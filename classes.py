9db4a1cdfeed6a742a2cdf1efb0d47a5b7457413
jensenk-0 <jensenk116@gmail.com>
2026-03-20 22:54:02 -0400

    added defending as a option and gave it to enemy too, made enemy ai smarter by a little bit, code needs a bit of cleaning though

--- classes.py
+++ classes.py
@@ -1,18 +1,20 @@
 # makes classes of player and enemy
 
 class Player:
-    def __init__(self, health, attack, defense, special_meter):
+    def __init__(self, health, attack, defense, defending, special_meter):
         self.health = health
         self.attack = attack
         self.defense = defense
+        self.defending = defending
         self.special_meter = special_meter
 
     
 class Enemy:
-    def __init__(self, health, attack, defense, special_meter):
+    def __init__(self, health, attack, defense, defending, special_meter):
         self.health = health
         self.attack = attack
         self.defense = defense
+        self.defending = defending
         self.special_meter = special_meter
 
 
--- turns.py
+++ turns.py
@@ -3,15 +3,30 @@ import random
 from classes import Player, Enemy
 
 
-player = Player(10, 10, 5, 5)
-enemy = Enemy(10, 10, 5, 5)
+player = Player(10, 10, 5, False, 0)
+enemy = Enemy(10, 10, 5, False, 0)
 
 def player_turn(turn):
-    player_input = input(f"turn {turn}\nplayer has {player.health} left\nenemy has {enemy.health} health left, what do you do?\nattack\nsurrender\n")
+    
+    if player.defending:
+        player.defense //= 2
+        player.defending = False
+        
+    print(player.defense)
+    player_input = input(f"turn {turn}\nplayer has {player.health} health left\nenemy has {enemy.health} health left, what do you do?\nattack\ndefend\nsurrender\n")
+    
     if player_input == "attack":
         dmg = dmg_cal(player, enemy)
         enemy.health -= dmg
         print(f"you did {dmg} damage to enemy")
+        
+
+    elif player_input == "defend":
+        print("player is defending, his defense doubled!")
+        defending(player)
+        print(player.defense)
+        
+        
 
     elif player_input == "surrender":
         player.health = 0
@@ -21,12 +36,25 @@ def player_turn(turn):
         
 
 
+enemy_max_hp = enemy.health
 
 def enemy_turn():
-    print("enemy attacks!")
-    dmg = dmg_cal(enemy, player)
-    player.health -= dmg
-    print(f"enemy did {dmg} damage to player")
+
+    if enemy.defending:
+        enemy.defense //= 2
+        enemy.defending = False
+
+    enemy_choice = random.randint(1, 2)
+
+    if enemy_choice == 1 or enemy.health == enemy_max_hp:
+        print("enemy attacks!")
+        dmg = dmg_cal(enemy, player)
+        player.health -= dmg
+        print(f"enemy did {dmg} damage to player")
+    
+    elif enemy_choice == 2:
+        defending(enemy)
+        print("The enemy is defending, his defense doubled!")
         
 
 def dmg_cal(attacker, defender):
@@ -36,3 +64,12 @@ def dmg_cal(attacker, defender):
         dmg = 0
 
     return dmg
+
+
+def defending(defender):
+    defender.defense *= 2
+    defender.defending = True
+
+
+
+
